af3345ad7380040cdb5b4c91525d9724ed21a09e
Alexandr Zhukov <derek@xhamster.com>
2019-08-30 15:44:54 +0300

    theme for app

--- src/apps/CampaignList/index.js
+++ src/apps/CampaignList/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import ReactDOM from 'react-dom';
+import theme from 'themes/theme.trafficstars';
 import CampaignListApp from './app';
 
-ReactDOM.render(<CampaignListApp />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <CampaignListApp />
+  </MuiThemeProvider>,
+  document.getElementById('root'),
+);
