bd71aa4c5b127363d59577267fa9b64e6e506527
Alexandr Zhukov <derek@xhamster.com>
2019-09-19 17:34:17 +0300

    search by email

--- src/apps/CampaignList/widgets/CampaignFilter/services/campaignFilterState/index.js
+++ src/apps/CampaignList/widgets/CampaignFilter/services/campaignFilterState/index.js
@@ -1,4 +1,5 @@
 import { useState, useContext } from 'react';
+import requestUser from './campaignFilterStateActions';
 import { CampaignListAppContext } from '../../../../services/CampaignListAppContext';
 import CAMPAIGN_LIST_TYPES_ACTIONS from '../../../../services/campaignListAppActionsType';
 
@@ -35,13 +36,22 @@ function useCampaignFilterState() {
       },
     });
   };
-  const onSubmitFilterHandler = event => {
+  const onSubmitFilterHandler = async event => {
     event.preventDefault();
-    const campaignId = Number(campaignFilterState.campaignId);
-    const type = campaignId
+    const { campaignId, email } = campaignFilterState;
+    const campaignIdNumnber = Number(campaignId);
+    const type = campaignIdNumnber
       ? CAMPAIGN_LIST_TYPES_ACTIONS.REQUEST_CAMPAIGN_ID
       : CAMPAIGN_LIST_TYPES_ACTIONS.REQUEST_MAIN_FILTER;
-    const payload = campaignId ? { campaignId } : campaignFilterState;
+    const payload = campaignIdNumnber
+      ? { campaignIdNumnber }
+      : { ...campaignFilterState };
+
+    if (email && !campaignId) {
+      payload.user_id = await requestUser({ email });
+    } else if (!email) {
+      payload.user_id = null;
+    }
 
     campaignListAppStateDispatch({
       type,
