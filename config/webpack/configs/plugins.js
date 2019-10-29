374768de205a433aa70b686e5d7eecda5b300e2a
Alexandr Zhukov <derek@trafficstars.com>
2019-10-29 10:10:29 +0200

    string replace loader plugin

--- config/webpack/configs/plugins.js
+++ config/webpack/configs/plugins.js
@@ -2,8 +2,26 @@ const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack
 const DashboardPlugin = require('webpack-dashboard/plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const customPaths = require('./paths');
 const { routes } = require('./config');
 
+/**
+ * Plugin @string-replace-loader
+ */
+const stringReplaceLoader = {
+  test: /constants\.js$/,
+  loader: 'string-replace-loader',
+  options: {
+    search: customPaths.i18nLocalesFilesPath,
+    replace: `${customPaths.rootStaticPublicPath.slice(0, -1)}${
+      customPaths.i18nLocalesFilesPath
+    }`,
+  },
+};
+
+/**
+ * Plugin @HtmlWebpackPlugin
+ */
 const pluginRoutes = routes.map(
   ({ name, filedirSrc, filedirDest, chunks }) => {
     return new HtmlWebpackPlugin({
@@ -16,8 +34,19 @@ const pluginRoutes = routes.map(
   },
 );
 
+/**
+ * Production plugins
+ */
 const productionPlugins = [...pluginRoutes];
 
+/**
+ * Module rules
+ */
+
+const moduleRules = [stringReplaceLoader];
+/**
+ * Common plugins
+ */
 const commonPlugins = [
   new DashboardPlugin(),
   new DuplicatePackageCheckerPlugin({
@@ -31,4 +60,5 @@ const commonPlugins = [
 module.exports = {
   productionPlugins,
   commonPlugins,
+  moduleRules,
 };
