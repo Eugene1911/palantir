66942c5e389a18e3465fdbad63e216d87aa56aee
Aleksey <al.nkow@gmail.com>
2019-10-29 11:59:06 +0300

    STARS-7283 theme provider

--- src/apps/CampaignList/index.js
+++ src/apps/CampaignList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import ReactDOM from 'react-dom';
 import theme from 'config/theme';
 import CampaignListApp from './app';
--- src/apps/MainApp.js
+++ src/apps/MainApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import mainAppRoutes from 'helpers/mainAppRoutes';
-import { ThemeProvider } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import theme from 'config/theme';
 
 function MainApp() {
