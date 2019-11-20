ae01ebf279b242dd301881acae79ab4306d65b09
Alexandr Zhukov <derek@trafficstars.com>
2019-11-20 16:37:30 +0200

    refact

--- src/helpers/numberFormat.js
+++ src/helpers/numberFormat.js
@@ -4,13 +4,13 @@ export function numberSiPrefix(number) {
   const u = Math.floor(+nn[1] / 3);
   return (
     nn[0] * Math.pow(10, +nn[1] - u * 3) +
-    ['p', 'n', 'u', 'm', '', 'k', 'M', 'G', 'T'][u + 4]
+    ['p', 'n', 'u', 'm', '', 'k', 'M', 'B', 'T'][u + 4]
   );
 }
 export const numberToFixed = (number, toFix) => {
   const convertNumber = Number(number);
 
-  if (!isNaN(convertNumber)) {
+  if (!Number.isNaN(convertNumber)) {
     return convertNumber.toFixed(toFix);
   }
 
