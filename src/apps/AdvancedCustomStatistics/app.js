48a1b62478d42a79f59a7d3b6c78775434e80dac
Alexandr Zhukov <derek@trafficstars.com>
2020-01-07 14:58:59 +0200

    refactoring with preload campaign and user data

--- src/apps/AdvancedCustomStatistics/app.js
+++ src/apps/AdvancedCustomStatistics/app.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import WrapperStartAppComponent from 'sharedComponents/WrapperStartAppComponent';
+import PreloadUserCampaign from 'sharedWidgets/PreloadUserCampaign';
 import AdvancedCustomStatisticsMain from './widgets/AdvancedCustomStatisticsMain';
-
 import AdvancedCustomStatisticsStore from './stores/AdvancedCustomStatisticsStore';
-import AdvancedCustomStatisticsFilterStore from './widgets/AdvancedCustomStatisticsFilter/store/AdvancedCustomStatisticsFilterStore';
-
-const store = {
-  advancedCustomStatisticsFilterStore: AdvancedCustomStatisticsFilterStore.create(),
-  advancedCustomStatisticsStore: AdvancedCustomStatisticsStore.create(),
-};
 
 function AdvancedCustomStatistics() {
   return (
-    <WrapperStartAppComponent store={store}>
-      <AdvancedCustomStatisticsMain />
-    </WrapperStartAppComponent>
+    <PreloadUserCampaign
+      ChildrenComonent={AdvancedCustomStatisticsMain}
+      InitStore={AdvancedCustomStatisticsStore}
+    />
   );
 }
 
