3c471cfb34fb887fdd3945a7d3701a70e22e1bce
Alexandr Zhukov <derek@trafficstars.com>
2020-01-07 11:41:36 +0200

    remove theme

--- src/sharedWidgets/ApplicationsAutocompleteMultiSelect/styles.js
+++ src/sharedWidgets/ApplicationsAutocompleteMultiSelect/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   main: {
     position: 'relative',
     height: '60px',
@@ -12,6 +12,6 @@ const useStyles = makeStyles(theme => ({
     left: 0,
     width: '100%',
   },
-}));
+});
 
 export default useStyles;
