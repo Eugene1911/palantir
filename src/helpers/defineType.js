8f89d4cddfb3ba9a558bb5e4f170a26cbf22a02c
Alexandr Zhukov <derek@trafficstars.com>
2020-01-07 11:39:13 +0200

    replace on lodash

--- src/helpers/defineType.js
+++
@@ -1,4 +0,0 @@
-export const isObject = item =>
-  Object.prototype.toString.call(item) === '[object Object]';
-export const isUndefined = item =>
-  Object.prototype.toString.call(item) === '[object Undefined]';
