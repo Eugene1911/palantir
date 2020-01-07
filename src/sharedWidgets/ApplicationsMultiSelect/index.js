cac6ada6a588578d6d5f92ffd4f0b4d01d2b9fc6
Alexandr Zhukov <derek@trafficstars.com>
2020-01-07 14:44:15 +0200

    refactoring application multi select

--- src/sharedWidgets/ApplicationsMultiSelect/index.js
+++ src/sharedWidgets/ApplicationsMultiSelect/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
@@ -7,84 +7,43 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
 import ApplicationsAutocompleteMultiSelect from 'sharedWidgets/ApplicationsAutocompleteMultiSelect';
+import useApplicationsMultiSelect from './services/useApplicationsMultiSelect';
 
-function ApplicationsMultiSelect() {
-  const [selectedApplications, setSelectedApplications] = useState(
-    [],
-  );
-  const onChangeApplication = (event, app) => {
-    if (app) {
-      const selectedIndex = selectedApplications.findIndex(
-        ({ id }) => id === app.id,
-      );
-
-      if (selectedIndex >= 0) {
-        selectedApplications.splice(selectedIndex, 1);
-      } else {
-        selectedApplications.push(app);
-      }
-
-      setSelectedApplications([...selectedApplications]);
-    }
-  };
-  const selectedApplicationsIds = selectedApplications.map(
-    ({ id }) => id,
-  );
-  const refAc = React.createRef();
+function ApplicationsMultiSelect({ applicationsIds, onChange }) {
+  const {
+    t,
+    selectedApplicationsIds,
+    selectedApplications,
+    onChangeApplicationHandler,
+    onChangeSeleactHandler,
+    onRenderValueHandler,
+    applicationsAutocompleteMultiSelectRef,
+  } = useApplicationsMultiSelect(applicationsIds, onChange);
 
   return (
     <FormControl fullWidth>
       <InputLabel shrink htmlFor="applications">
-        Applications
+        {t('common:form.applications')}
       </InputLabel>
       <Select
         displayEmpty
         name="applications"
         multiple
-        onChange={(event, { props }) => {
-          const { value } = props;
-          if (value === 'all') {
-            setSelectedApplications([]);
-          } else {
-            onChangeApplication(null, { id: props.value });
-          }
-        }}
-        renderValue={selected => {
-          if (!selected || !selected.length)
-            return <Typography noWrap>All</Typography>;
-
-          const selectedLength = selected.length;
-
-          if (selectedLength > 4) {
-            return (
-              <Typography noWrap>
-                {`Selected applications: ${selectedLength}`}
-              </Typography>
-            );
-          }
-
-          const applicationsSelectedList = selectedApplications
-            .filter(({ id }) => selected.includes(id))
-            .map(({ name }) => name)
-            .join(', ');
-
-          return (
-            <Typography noWrap>{applicationsSelectedList}</Typography>
-          );
-        }}
+        onChange={onChangeSeleactHandler}
+        renderValue={onRenderValueHandler}
         value={selectedApplicationsIds}
       >
         <ApplicationsAutocompleteMultiSelect
-          ref={refAc}
+          ref={applicationsAutocompleteMultiSelectRef}
           selectedApplicationsIds={selectedApplicationsIds}
-          onChange={onChangeApplication}
+          onChange={onChangeApplicationHandler}
         />
         <MenuItem value="all">
           <Checkbox
             checked={!selectedApplicationsIds.length}
             indeterminate
           />
-          <ListItemText primary="All" />
+          <ListItemText primary={t('common:form.all')} />
         </MenuItem>
         {selectedApplications.map(app => (
           <MenuItem key={app.id} value={app.id}>
@@ -96,5 +55,13 @@ function ApplicationsMultiSelect() {
     </FormControl>
   );
 }
+
+ApplicationsMultiSelect.propTypes = {
+  applicationsIds: PropTypes.array,
+  onChange: PropTypes.func.isRequired,
+};
+ApplicationsMultiSelect.defaultProps = {
+  applicationsIds: [],
+};
 
 export default ApplicationsMultiSelect;
