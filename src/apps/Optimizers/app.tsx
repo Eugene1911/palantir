e2d73c257e1eb5be2ab505398721c5bb81b82c33
Alexandr Zhukov <derek@trafficstars.com>
2020-10-28 19:36:23 +0200

    harcode path

--- src/apps/Optimizers/app.tsx
+++ src/apps/Optimizers/app.tsx
@@ -17,7 +17,7 @@ import ChooseRulesStore from './widgets/ChooseRules/store/ChooseRulesStore';
 import StoreProvider from './StoreProvider';
 
 function OptimizersApp(): JSX.Element {
-  const { path = '/' } = useRouteMatch();
+  const { path = '/advertisers/optimizers/' } = useRouteMatch();
   const routerModel = RouterModel.create();
   const history = syncHistoryWithStore(
     createBrowserHistory(),
