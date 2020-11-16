6f4849b60b258ed8aa3f9acb7cad8160900496d6
Рихтер Андрей <airikhter@domclick.ru>
2020-11-16 11:45:15 +0300

    add classes to ExpandIcon

--- src/apps/NewCampaignSettings/widgets/CampaignSettings/components/CustomDrawer/components/ExpandIcon/index.tsx
+++ src/apps/NewCampaignSettings/widgets/CampaignSettings/components/CustomDrawer/components/ExpandIcon/index.tsx
@@ -4,6 +4,7 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 // eslint-disable-next-line import/no-cycle
 import { IFilterCategoryItem } from '../ListCategory';
+import useStyles from './useStyles';
 
 interface IExpandIconProps {
   isSomeFilledListItem: boolean;
@@ -20,6 +21,8 @@ const ExpandIcon = ({
   openAsyncFilter,
   category,
 }: IExpandIconProps): JSX.Element => {
+  const classes = useStyles();
+
   if (isAsyncLoadingList) {
     if (!isShowAsyncLoadButton) {
       return null;
@@ -33,9 +36,9 @@ const ExpandIcon = ({
 
   return (
     <ExpandMoreIcon
-      style={{
-        background: isSomeFilledListItem ? '#e7e8f7' : 'transparent',
-      }}
+      className={
+        isSomeFilledListItem ? classes.filledIcon : classes.icon
+      }
     />
   );
 };
