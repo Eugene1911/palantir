c69ebeaffa96a2f18d806a2635944bbb5be6fe20
Рихтер Андрей <airikhter@domclick.ru>
2020-11-18 18:19:03 +0300

    fix bug with notifications

--- src/apps/MainApp.js
+++ src/apps/MainApp.js
@@ -1,35 +1,25 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import mainAppRoutes from 'helpers/mainAppRoutes';
-import { SnackbarProvider } from 'notistack';
-import { MAX_COUNT_SNACK } from 'config/constants';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from 'config/theme';
 
 function MainApp() {
   return (
-    <SnackbarProvider maxSnack={MAX_COUNT_SNACK}>
-      <ThemeProvider theme={theme}>
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: '1570px',
-          }}
-        >
-          <Router>
-            <Switch>
-              {mainAppRoutes.map(route => (
-                <Route key={route.path} {...route} />
-              ))}
-            </Switch>
-          </Router>
-        </div>
-      </ThemeProvider>
-    </SnackbarProvider>
+    <ThemeProvider theme={theme}>
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: '1570px',
+        }}
+      >
+        <Router>
+          {mainAppRoutes.map(route => (
+            <Route key={route.path} {...route} />
+          ))}
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/apps/NewCampaignSettings/widgets/CampaignSettings/widgets/SaveStepActions/index.tsx
+++ src/apps/NewCampaignSettings/widgets/CampaignSettings/widgets/SaveStepActions/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
 import CampaignActions from 'sharedWidgets/CampaignActions';
 import { LoadingStatus } from 'sharedTypes';
@@ -18,11 +18,15 @@ const SaveStepAction = ({
   saveActions,
 }: ISaveStepActionProps): JSX.Element => {
   const infoNotification = useHookInfoNotification();
-  const history = useHistory();
+  // const history = useHistory();
 
   const handleSaveAsDraft = (link: string): void => {
     const resultData = getNewCampaignSettingsResultData();
-    const successCallback = (): void => history.push(link);
+    // TODO разобраться потом с редиректом и показом уведомления на сохранение
+    // const successCallback = (): void => history.push(link);
+    const successCallback = (): void => {
+      console.log(link);
+    };
     saveActions.saveCampaign(
       infoNotification,
       resultData,
