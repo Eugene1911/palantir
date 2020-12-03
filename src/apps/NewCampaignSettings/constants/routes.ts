98897597ee18cc7d7b55cc71d3e2077cb76cef8b
Рихтер Андрей <airikhter@domclick.ru>
2020-12-03 17:51:29 +0300

    add routing to index.tsx

---
+++ src/apps/NewCampaignSettings/constants/routes.ts
@@ -0,0 +1,10 @@
+import NewCampaignSettings from '../app';
+
+const routes = [
+  {
+    component: NewCampaignSettings,
+    path: '/:mode?/:id?',
+  },
+];
+
+export default routes;
--- src/apps/NewCampaignSettings/index.tsx
+++ src/apps/NewCampaignSettings/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import ReactDOM from 'react-dom';
 import theme from 'config/theme';
-import NewCampaignSettings from './app';
+import routes from './constants/routes';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <NewCampaignSettings />
+    <Router basename="new_create_campaign_settings">
+      <Router>
+        {routes.map(route => (
+          <Route
+            key={route.path}
+            path={route.path}
+            component={route.component}
+          />
+        ))}
+      </Router>
+    </Router>
   </ThemeProvider>,
   document.getElementById('root'),
 );
