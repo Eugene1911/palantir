e6333180d6aae2ce115ee9733c0c050f295e3e18
Alexandr Zhukov <derek@trafficstars.com>
2020-12-16 15:17:12 +0200

    fixed path for route

--- src/helpers/pagePath.js
+++ src/helpers/pagePath.js
@@ -9,7 +9,7 @@ const PAGE_PATH = {
   CLIENT_LOGIN_AS: '/backoffice/login/user/',
 
   // Campaign creating
-  CAMPAIGN_SETTINGS: '/new_create_campaign_settings', // 1 step
+  CAMPAIGN_SETTINGS: 'new_create_campaign_settings', // 1 step
   CAMPAIGN_AUDIENCE_AND_PRICING:
     '/new_create_campaign_audience_pricing', // 2 step
 };
